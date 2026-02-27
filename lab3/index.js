e517342490f153c3b150bc32198230aa940a3be8
Bohdan web2026-3 <w1nzeen.r@gmail.com>
2026-02-27 21:30:11 +0200

    added some changes

---
+++ lab3/index.js
@@ -0,0 +1,22 @@
+const { program } = require('commander');
+const fs = require('fs');
+
+program
+.option('-i, --input <path>', 'шлях до файлу для читання')
+.option('-o, --output <path>', 'шлях до файлу для запису')
+.option('-d, --display', 'вивести у консоль')
+
+program.parse(process.argv);
+const options = program.opts();
+
+if (!options.input) {
+  console.error('Please, specify input file');
+  process.exit(1); 
+}
+
+if (!fs.existsSync(options.input)) {
+  console.error('Cannot find input file');
+  process.exit(1);
+}
+
+console.log('Отримані параметри:', options);
